e15c2cc67a174f800ffb11a60f7b65c0881a8a69
venomousmoog <venomousmoog@users.noreply.github.com>
2026-05-20 14:57:39 -0700

    shared-tmux update

--- scripts/shared-tmux.sh
+++ scripts/shared-tmux.sh
@@ -2,4 +2,5 @@
 
 mkdir /tmp/tmux-shared
 tmux -S /tmp/tmux-shared/shared new-session -d -s auto
+tmux -S /tmp/tmux-shared/shared set-option -g default-command bash
 chmod 777 /tmp/tmux-shared/shared
